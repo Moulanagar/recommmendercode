277d6a7fa7c387216c27c21adfb3d6d7ed7b62da
Moulanagar <arhamequbal123@gmail.com>
2025-06-10 00:25:38 +0530

    neww

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ from app.api.v1.routes import routes
 app = FastAPI()
 
 app.include_router(routes.router, prefix="/api/v1")
+
+
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
